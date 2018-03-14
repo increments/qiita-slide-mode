0e468e8178e1f99c1eeda269c34053cd14b3e0c9
Jorge Bucaran <mail@jorgebucaran.com>
2018-03-14 13:51:39 +0900

    Remove namespace and add slide and SlideViewer. Thanks @yuku-t.

--- index.d.ts
+++ index.d.ts
@@ -1,28 +1,35 @@
-export namespace SlideMode {
-  /**
-   * Interface for SlideMode state.
-   */
-  export interface State {
-    isFullScreen: boolean
+/**
+ * Interface for SlideMode state.
+ */
+export interface State {
+  isFullScreen: boolean
+  page: number
+  tooltip: {
     page: number
-    tooltip: {
-      page: number
-      left: number
-      isVisible: boolean
-    }
-  }
-
-  /**
-   * Interface for SlideMode actions.
-   */
-  export interface Actions {
-    tooltip: {
-      show(props: { page: number; left: number }): State
-      hide(): State
-    }
-    toggleFullScreen(): State
-    prev(): State
-    next(length: number): State
-    goto(page: number): State
+    left: number
+    isVisible: boolean
   }
 }
+
+/**
+ * Interface for SlideMode actions.
+ */
+export interface Actions {
+  tooltip: {
+    show(props: { page: number; left: number }): State
+    hide(): State
+  }
+  toggleFullScreen(): State
+  prev(): State
+  next(length: number): State
+  goto(page: number): State
+}
+
+export declare const slide: {
+  state: State
+  actions: Actions
+}
+
+export declare const SlideViewer: (
+  props: { state: State; actions: Actions; pages: string[] }
+) => any
