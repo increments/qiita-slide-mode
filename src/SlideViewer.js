ab389d790d397ba736747b47646f5f9e6d3e1a2e
HANATANI Takuma <mail@potato4d.me>
2018-03-14 18:28:08 +0900

    Install QiitaLogo and Styling Outline

--- src/SlideViewer.js
+++ src/SlideViewer.js
@@ -1,11 +1,13 @@
 import { h } from "hyperapp"
 import { Tooltip } from "./Tooltip"
 import { getMagnitudeFromRange } from "./getMagnitudeFromRange"
+import { QiitaLogo } from "./QiitaLogo"
 
 export const SlideViewer = ({ state, actions, pages }) => (
-  <div class={"slide unselectable" + " " + (state.isFullScreen ? "fullscreen" : "")}>
-    <div class="slideViewer">
+  <div class={"slideMode unselectable" + " " + (state.isFullScreen ? "fullscreen" : "")}>
+    <div class="slideMode-Viewer">
       <div
+        class="slideMode-Viewer_content"
         onclick={event => {
           if (event.target.tagName !== "IMG" || event.target.tagName === "A") {
             return 
@@ -28,23 +30,29 @@ export const SlideViewer = ({ state, actions, pages }) => (
 
     <Tooltip state={state.tooltip} length={pages.length} />
 
-    <div class="dashboard">
-      <button disabled={state.page === 0} onclick={actions.prev}>
-        Prev
+    <div class="slideMode-Dashboard">
+      <button
+        class={"slideMode-Dashboard_button slideMode-Dashboard_button--prev " + (state.page !== 0 ? "slideMode-Dashboard_button--clickable" : "")}
+        disabled={state.page === 0}
+        onclick={actions.prev}>
+        <i class="fa fa-backward" />
       </button>
       <button
+        class={"slideMode-Dashboard_button slideMode-Dashboard_button--next " + (state.page !== pages.length - 1 ? "slideMode-Dashboard_button--clickable" : "")}
         disabled={state.page === pages.length - 1}
         onclick={() => actions.next(pages.length)}
       >
-        Next
+        <i class="fa fa-forward" />
       </button>
 
-      <span>
-        {state.page + 1}/{pages.length}
+      <span
+        class="slideMode-Dashboard_pageCount"
+      >
+        {state.page + 1} / {pages.length}
       </span>
 
       <div
-        class="progress"
+        class="slideMode-Dashboard_progress"
         onmousemove={event => {
           actions.tooltip.show({
             page: getMagnitudeFromRange(
@@ -69,14 +77,20 @@ export const SlideViewer = ({ state, actions, pages }) => (
         }}
       >
         <div
-          class="progressFill"
+          class="slideMode-Dashboard_progressFill"
           style={{
             width: `${(state.page + 1) / pages.length * 100}%`
           }}
         />
       </div>
 
-      <button onclick={actions.toggleFullScreen}>■</button>
+      <button
+        class="slideMode-Dashboard_button slideMode-Dashboard_button--fullscreen slideMode-Dashboard_button--clickable"
+        onclick={actions.toggleFullScreen}>
+        <i class="fa fa-desktop" />
+      </button>
+
+      <QiitaLogo />
     </div>
   </div>
 )
