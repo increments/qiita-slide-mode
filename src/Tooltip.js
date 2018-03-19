c60f096d699f9a33fa7149f6872949f7762e6cf9
HANATANI Takuma <mail@potato4d.me>
2018-03-19 10:53:18 +0900

    Refactor class name

--- src/Tooltip.js
+++ src/Tooltip.js
@@ -2,7 +2,7 @@ import { h } from "hyperapp"
 
 export const Tooltip = ({ state, length }) =>
   state.isVisible && (
-    <div style={{ left: `${state.left}px` }} class={"tooltip"}>{`${
+    <div style={{ left: `${state.left}px` }} class={"slideMode-Tooltip"}>{`${
       state.page
     }/${length}`}</div>
   )
