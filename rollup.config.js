d4a01e370316229611eb241027e0053badfd1731
Yuku TAKAHASHI <yuku@qiita.com>
2018-04-18 17:42:38 +0900

    Distribute cjs file

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,15 @@
 import buble from "rollup-plugin-buble"
 import commonjs from "rollup-plugin-commonjs"
-import resolve from "rollup-plugin-node-resolve"
-import uglify from "rollup-plugin-uglify"
 
 export default {
-  plugins: [buble({ jsx: "h" }), resolve(), commonjs(), uglify()]
+  input: "./src/index.js",
+  output: {
+    file: "./dist/index.js",
+    format: "cjs",
+    sourcemap: true,
+  },
+  plugins: [
+    commonjs(),
+    buble({ jsx: "h" })
+  ]
 }
